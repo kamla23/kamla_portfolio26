8102057bd2fc2ab3d7272d52cc4b266f4fe8e62c
Kamla23 <kamlabai23@navgurukul.org>
2026-05-15 21:44:44 +0530

    This is api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Aapka backend URL
+  baseURL: "https://kamla-portfolio26.onrender.com",
 });
 
-// Ye middleware har request ke sath token automatic bhej dega agar localstorage mein hai toh
+
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('token')) {
     req.headers['x-auth-token'] = localStorage.getItem('token');
